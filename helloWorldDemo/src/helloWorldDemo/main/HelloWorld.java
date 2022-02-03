8da0f0fd2687abe377809eb40c6ef19f3f863efd
jthakur29 <jahanvi.thakur29@gmail.com>
2022-02-03 18:02:55 +0530

    DONE

---
+++ helloWorldDemo/src/helloWorldDemo/main/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package helloWorldDemo.main;
+
+/**
+ * @author croma
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("hello everyone, welcome to eclipse");
+	}
+
+}
